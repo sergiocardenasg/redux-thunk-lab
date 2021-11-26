7ecf4ca3547671f378dc6dd278188b7973d90d7f
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-26 13:34:05 -0700

    commit tree cfd892d8ea880146ca0754dd3cda65c907612949

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
 // write your CatList component here
+import React, { PureComponent } from 'react'
+
+export default class CatList extends PureComponent {
+    listCats = () => {
+        return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+    }
+
+    render() {
+        return (
+            <div>
+                {this.listCats()}
+            </div>
+        )
+    }
+}
